6a8d571c09ddef5357af4a79d8d9f71bb059e813
aoao <koki.kurozu@gmail.com>
2020-10-18 18:04:23 +0900

    10/18

--- scrape_part3/save_mysql.py
+++ scrape_part3/save_mysql.py
@@ -2,7 +2,7 @@ import MySQLdb
 
 # MySQLサーバーに接続し、コネクションを取得する。
 # ユーザー名とパスワードを指定してscrapingデータベースを使用する。接続に使用する文字コードはutf8mb4とする。
-conn = MySQLdb.connect(db='scraping', user='scraper', passwd='password', charset='utf8mb4')
+conn = MySQLdb.connect(database='test_newdb', user='root', password='&ka4n55jou6c@2', charset='utf8mb4')
 
 c = conn.cursor()  # カーソルを取得する。
 # execute()メソッドでSQL文を実行する。
